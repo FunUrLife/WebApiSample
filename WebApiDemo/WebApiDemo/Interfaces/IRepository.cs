b78bd9f0a0862c9c4fba1b6976fefc3b9ca16943
JusTryIt <qmtsgx9304@gmail.com>
2019-10-19 11:36:17 +0800

    Init BaseRepository

---
+++ WebApiDemo/WebApiDemo/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Interfaces
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Get();
+        IEnumerable<T> Find(Expression<Func<T, bool>> query);
+        T Get(int id);
+        void Post(T item);
+        void Put(T item);
+        void Delete(T item);
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Repositories/BaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using WebApiDemo.Interfaces;
+
+namespace WebApiDemo.Repositories
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private const string ConnectionString = "your connection string here";
+
+        protected readonly IDbConnection connection;
+
+        public BaseRepository()
+        {
+
+        }
+
+        public abstract void Delete(T item);
+        public abstract IEnumerable<T> Find(Expression<Func<T, bool>> query);
+        public abstract IEnumerable<T> Get();
+        public abstract T Get(int id);
+        public abstract void Post(T item);
+        public abstract void Put(T item);
+    }
+}

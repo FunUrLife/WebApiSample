c2a5fc20b5e668e6e21c12f45579edc5a8685999
JusTryIt <qmtsgx9304@gmail.com>
2019-10-19 13:11:45 +0800

    Update : DemoRepoUnitTest

--- WebApiDemo/WebApiDemo.Tests/DemoRepoUnitTest.cs
+++ WebApiDemo/WebApiDemo.Tests/DemoRepoUnitTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WebApiDemo.Tests
 {
@@ -14,10 +13,12 @@ namespace WebApiDemo.Tests
             // Arrange
             var _Repo = new Repositories.DemoRepository();
             // Act
-            var expected = new DataModels.EmpDataModel { ID = ID_RESULT, NAME = "詹竣皓" };
+            var expected = new DataModels.EmpDataModel { ID = ID_RESULT, NAME = "詹竣皓", NO = "001" };
             var actual = _Repo.Get(ID_RESULT);
             // Assert
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.ID, actual.ID);
+            Assert.AreEqual(expected.NAME, actual.NAME);
+            Assert.AreEqual(expected.NO, actual.NO);
         }
     }
 }

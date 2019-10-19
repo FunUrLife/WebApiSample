7e6d91ee944ac0a0b39b5aaca16a2f3cdaf8dc42
JusTryIt <qmtsgx9304@gmail.com>
2019-10-19 12:20:09 +0800

    Add : Extensions

---
+++ WebApiDemo/WebApiDemo/Extensions/ConnExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace WebApiDemo.Extensions
+{
+    internal static class ConnExtension
+    {
+        internal static bool TryConnect(this IDbConnection conn)
+        {
+            if (conn == null)
+                throw new ArgumentNullException("IDbConnection Is Null");
+
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            return conn.State == ConnectionState.Open;
+        }
+    }
+}

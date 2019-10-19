b1615eb9a2d9a6f9068cdecf528a294a08dc9150
JusTryIt <qmtsgx9304@gmail.com>
2019-10-19 12:48:41 +0800

    Update : DemoController connect to database

--- WebApiDemo/WebApiDemo/Controllers/DemoController.cs
+++ WebApiDemo/WebApiDemo/Controllers/DemoController.cs
@@ -4,36 +4,54 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiDemo.Repositories;
+using WebApiDemo.DataModels;
+using Newtonsoft.Json;
 
 namespace WebApiDemo.Controllers
 {
     public class DemoController : ApiController
     {
+        DemoRepository _Repo;
+
+        public DemoController() => _Repo = new DemoRepository();
         // GET: api/Demo
-        public IEnumerable<string> Get()
+        public IEnumerable<EmpDataModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _Repo.Get();
         }
 
         // GET: api/Demo/5
-        public string Get(int id)
+        public EmpDataModel Get(int id)
         {
-            return "value";
+            var item = _Repo.Get(id);
+            if (item == null)
+                BadRequest("Item Not Found");
+            return item;
         }
 
         // POST: api/Demo
         public void Post([FromBody]string value)
         {
+            var item = JsonConvert.DeserializeObject<EmpDataModel>(value);
+
+            _Repo.Post(item);
         }
 
         // PUT: api/Demo/5
-        public void Put(int id, [FromBody]string value)
+        public void Put([FromBody]string value)
         {
+            var item = JsonConvert.DeserializeObject<EmpDataModel>(value);
+
+            _Repo.Put(item);
         }
 
         // DELETE: api/Demo/5
-        public void Delete(int id)
+        public void Delete([FromBody]string value)
         {
+            var item = JsonConvert.DeserializeObject<EmpDataModel>(value);
+
+            _Repo.Delete(item);
         }
     }
 }

5219280682e14182d9581eef6b3628eb8805f758
JusTryIt <qmtsgx9304@gmail.com>
2019-10-19 13:03:35 +0800

    init DemoRepoUnitTest

---
+++ WebApiDemo/WebApiDemo.Tests/DemoRepoUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApiDemo.Tests
+{
+    [TestClass]
+    public class DemoRepoUnitTest
+    {
+        [TestMethod]
+        public void Test_Get()
+        {
+            const int ID_RESULT = 1;
+
+            // Arrange
+            var _Repo = new Repositories.DemoRepository();
+            // Act
+            var expected = new DataModels.EmpDataModel { ID = ID_RESULT, NAME = "詹竣皓" };
+            var actual = _Repo.Get(ID_RESULT);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
